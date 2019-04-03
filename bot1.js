8d5a17822a36ac6847f687615c47ae794b62c283
judy-chen <vaelyra@gmail.com>
2019-04-02 17:17:02 -0700

    started working on embed

--- bot1.js
+++ bot1.js
@@ -9,12 +9,6 @@ var ninjaAPI = new NinjaAPI({
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
-    console.log("```css " + "Celestial Hideout : The Shaper's Realm [T17] \n" +
-    "Celestial Hideout : The Shaper's Realm [T17] \n" +
-    "Alpline Hideout : Summit Map [T13] : Very Rare \n" +
-    "Divided Hideout : The Twilight Temple : Rare \n" +
-    "Haunted Hideout : Haunted Mansion Map [T2] : Extremely Rare \n" +
-    "Sanguine Hideout : Crimson Temple Map [T13] : Extremely Rare \n" + "```")
 })
 //\n \ Alpline Hideout : Summit Map [T13] : Very Rare \n \ Divided Hideout : The Twilight Temple : Rare \n \ ```"
 
@@ -35,6 +29,8 @@ client.on('guildMemberAdd', member => {
 		+ '\n' + '```css' + '\n' + 'Join us over in #general chat! ```');
 
 	});
+
+
 
   client.on("message", message => {
     if (message.content.trim() == ("!hideout")){
@@ -58,8 +54,48 @@ client.on('guildMemberAdd', member => {
     else if (message.content.trim() == "!ninja") {
       ninjaAPI.update()
         .then((result) => {
-          console.log("Updated data, here are the results of the requests:", result);
+          console.log("Updated data, here are the results of the requests:", result[0].data.lines)
+          var i;
+          for (i = 0; i < result[0].data.lines.length; i++) {
+            if (result[0].data.lines[i].pay && result[0].data.lines[i].receive) {
+            var payvalue = calcRatio(result[0].data.lines[i].pay.value);
+            name = result[0].data.lines[i].currencyTypeName
+            receivevalue = result[0].data.lines[i].receive.value;
+            console.log('Sell: ' + name + " : " + payvalue);
+            console.log('Buy: ' + name + " : " + receivevalue);
+
+            }
+          }
+          message.channel.send({embed});
+          // console.log(ninjaAPI.getCurrencyDetails("Mirror of Kalandra"));
+          // console.log(ninjaAPI.getCurrencyDetails("blessing-of-chayula"));
+          // console.log(ninjaAPI.getCurrencyDetails("Orb of Alchemy"));
           return ninjaAPI.save();
         })
     }
        });
+
+function calcRatio(value) {
+  return (1 / parseFloat(value)).toFixed(1);
+}
+
+const embed = new Discord.RichEmbed()
+  .setDescription("? x <:chaos:562076109865484289>   => 1.0 x <:mirror:562221382394445845>  \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:exa:562076209991647233> \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:annul:562075957289287681> \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:ancient:562627051065049107> \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:divine:562076191490703380> \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:harbinger:562626908047540225>  \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:redsextant:562076565773615116>  \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:yellowsextant:562076541836722206>   \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:splinter:562627892090109962> \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:vaal:562076451260858368> \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:whitesextant:562075981830160385>  \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:regal:562076374949691402>  \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:gcp:562076303658975242> \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:regret:562076401524801557> \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:chisel:562076133391204361>  \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:fusing:562076245660008459>  \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:alch:562075893900509195>  \n" +
+                  "? x <:chaos:562076109865484289>   => 1.0 x :<:scour:562076420134797326> \n"  +
+                  "? x <:chaos:562076109865484289>   => 1.0 x <:blessed:562076035253010442>");
